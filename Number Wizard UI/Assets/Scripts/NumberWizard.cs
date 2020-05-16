386d4e328eb388c1e6d2a3f30eaf2271edbaf273
Levent <lvntbkdmr@gmail.com>
2020-05-16 23:19:24 +0300

    game UI update

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = max + 1;
+        guess = (max + min) / 2;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+    }
+}
--- Number Wizard UI/README.md
+++ Number Wizard UI/README.md
@@ -5,3 +5,5 @@
 * Different Sceens with different buttons
 * Button click -> link with an empty game object which has a public method, so onClick() event triggers that public method inside game object's relative c# script
 * SceneManager.LoadScene() 
+* Try to Strect anchor points of texts inside buttons
+* Use UI Image element as backgrounds
